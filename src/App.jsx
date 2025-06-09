e1fe320f81a98732f906d04ecf104144270c9620
Ayush Mittal <mittalayush0703@gmail.com>
2025-06-09 19:12:03 +0530

    created the routing functionality and error handling of wrong routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,40 @@
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import "./App.css";
+import { About } from "./pages/About";
+import { Home } from "./pages/Home";
+import { Contact } from "./pages/Contact";
+import { Country } from "./pages/Country";
+import { AppLayout } from "./components/Layout/AppLayout";
+import { ErrorPage } from "./pages/ErrorPage";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <AppLayout />,
+    errorElement: <ErrorPage />,
+    children:[
+      {
+        path: "/",
+        element: <Home />,
+      },
+      {
+        path: "about",
+        element: <About />,
+      },
+      {
+        path: "contact",
+        element: <Contact />,
+      },
+      {
+        path: "country",
+        element: <Country />,
+      }
+    ]
+  }
+])
+
 const App = ()=>{
-  return <h1>App page</h1>
+  return <RouterProvider router={router}></RouterProvider>
 }
 
 export default App;
