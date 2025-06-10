2e628ec5180eaf438dc9677b4750aaf9574b779f
Ayush Mittal <mittalayush0703@gmail.com>
2025-06-10 16:14:04 +0530

    added the functionality in contact page

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,3 +1,38 @@
+import countryFacts from "../API/countryData.json"
+
 export const About = ()=>{
-    return <h1>About page</h1>
+    return (
+        <section className="section-about container">
+            <h2 className="container-title">
+                Here are some Interesting Facts
+                <br />
+                we're Proud of
+            </h2>
+            <div className="gradient-cards">
+                {
+                    countryFacts.map((curElem)=>{
+                        return (
+                            <div className="card" key={curElem.id}>
+                                <div className="container-card bg-blue-box">
+                                    <p className="card-title">{curElem.countryName}</p>
+                                    <p>
+                                        <span className="card-description">Capital:</span>
+                                        {curElem.capital}
+                                    </p>
+                                    <p>
+                                        <span className="card-description">Population:</span>
+                                        {curElem.population}
+                                    </p>
+                                    <p>
+                                        <span className="card-description">InterestingFact:</span>
+                                        {curElem.interestingFact}
+                                    </p>
+                                </div>
+                            </div>
+                        )
+                    })
+                }
+            </div>
+        </section>
+    )
 }
--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,3 +1,20 @@
-export const Contact = ()=>{
-    return <h1>Contact page</h1>
+export const Contact = () => {
+    const handleFormSubmit = (formData)=>{
+        const formInputData = Object.fromEntries(formData.entries());
+        console.log(formInputData);
+    }
+
+    return (
+        <section className="section-contact">
+            <h2 className="container-title">Contact Us</h2>
+            <div className="container contact-wrapper">
+                <form action={handleFormSubmit}>
+                    <input type="text" required autoComplete="false" placeholder="Enter your name" name="username" />
+                    <input type="email" required autoComplete="false" placeholder="Enter your email" name="email" className="form-control" />
+                    <textarea className="form-control" rows="10" placeholder="Enter your message" name="message" required autoComplete="false"></textarea>
+                    <button type="submit">Send</button>
+                </form>
+            </div>
+        </section>
+    )
 }
